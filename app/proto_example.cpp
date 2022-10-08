1b024825a8acff8202d5401a5ae6b85b00381495
Mohammad Hossein Heydarchi <mhheidarchi2003@yahoo.com>
2022-10-08 18:48:29 +0200

    Complete ProtoUtility class implementation Utilized ProtoUtility class in main()

--- app/proto_example.cpp
+++ app/proto_example.cpp
@@ -1,31 +1,46 @@
 #include <fstream>
 #include <iostream>
 #include "lib/message.pb.h"
+#include "lib/proto_utility.h"
 
 
 using namespace std;
-
-class ProtoUtility{
-
-public:
-    ProtoUtility(){}
-
-    bool readFromFile(){
-
-        return true;
-    }
-
-    bool writeToFile(){
-
-         return true;
-    }
-
-
-};
-
+using namespace proto_example;
 
 int main(){
-    cout<<"ProtoBuf example starting ...";
+    cout<<"ProtoBuf example starting ..."<<endl;
 
-    return 0;
+    Message msg;
+    Message msg_read;
+    ProtoUtility protoUtility;
+
+    msg.set_id(2);
+    msg.set_title("TestMessage");
+    msg.set_date_time("2022-10-08");
+    msg.set_sender_name("M.H.H");
+    msg.set_priority(Message_Priority::Message_Priority_REGULAR);
+
+    Message_OtherReceiver* receiver1=msg.add_receivers();
+    receiver1->set_receiver_name("PersonNo-1");
+    Message_OtherReceiver* receiver2=msg.add_receivers();
+    receiver2->set_receiver_name("PersonNo-2");
+
+    cout<<endl<<"Write to file ..."<<endl;
+
+    protoUtility.writeToFile((const char*)"test.bin",&msg);
+
+    protoUtility.readFromFile((const char*)"test.bin",&msg_read);
+
+    cout<<endl<<"Read from file ..."<<endl;
+    cout<<"Title :"<<msg_read.title()<<endl;
+    cout<<"Id :"<<msg_read.id()<<endl<<endl;
+    cout<<"ReceiverName size :"<<msg_read.receivers_size()<<endl;
+    for(int i=0;i<msg_read.receivers_size();i++)
+    {
+            const Message_OtherReceiver& receiver=msg_read.receivers(i);
+            cout<<"receiver_name :"<<receiver.receiver_name()<<endl;
+    }
+
+    google::protobuf::ShutdownProtobufLibrary();
+
 }
---
+++ lib/proto_utility.h
@@ -0,0 +1,37 @@
+#include<fstream>
+#include <iostream>
+#include "lib/message.pb.h"
+
+using namespace std;
+
+namespace proto_example{
+class ProtoUtility{
+
+public:
+    ProtoUtility(){}
+
+    bool readFromFile(const char* filename, Message* msg){
+            fstream  ifile(filename, ios::binary | ios::in);
+            if (!msg->ParseFromIstream(&ifile)) {
+                cerr << "Failed to parse Message." << endl;
+                return -1;
+            }
+            cout<<"Read successfully !"<<endl;
+            return true;
+
+    }
+
+    bool writeToFile(const char* filename, Message* msg){
+            fstream  ofile(filename, ios::binary | ios::out  | ios::trunc);
+            if (!msg->SerializeToOstream(&ofile)) {
+                cerr << "Failed to write Message." << endl;
+                return false;
+            }
+            cout<<"Written successfully !"<<endl;
+            return true;
+
+    }
+
+
+};
+}
